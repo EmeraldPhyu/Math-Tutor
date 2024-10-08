0e3216644d8fe6c0e71a7407e5196272fab92cb6
Emerald phyu hnin htway <phyuhnin.htway@dzhintl.com>
2024-10-08 11:28:15 +0800

    Initial Commit

---
+++ Math Tutor/Math_TutorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Math_TutorApp.swift
+//  Math Tutor
+//
+//  Created by Emerald on 8/10/24.
+//
+
+import SwiftUI
+
+@main
+struct Math_TutorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
